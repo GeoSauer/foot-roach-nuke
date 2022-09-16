452b8fd1fa1cf38a449b5e388d7fae0f228fc4e7
GeoSauer <geoffrey.sauer89@gmail.com>
2022-09-16 12:39:46 -0600

    refine some displayState conflicts, clean up display functions

--- app.js
+++ app.js
@@ -15,26 +15,34 @@ const opponents = ['boot', 'roach', 'nuke'];
 let gameState = 'pick'; // 'pick', or 'results'
 let pick = ''; // 'boot', 'roach', 'nuke'
 let opponentPick = ''; // 'boot', 'roach', 'nuke'
-let result = ''; // 'win', 'lose', or 'draw'
+// let result = ''; // 'win', 'lose', or 'draw'
+
+// let wins = 0;
+// let losses = 0;
+// let draws = 0;
 
 /* Actions */
 function loadPage() {
     displayPick();
     displayResults();
+    // displayScoreboard();
 }
 
 function battle(userPick) {
     gameState = 'results';
     pick = userPick;
     opponentPick = getRandomItem(opponents);
-
+    console.log(opponentPick);
     if (pick === opponentPick) {
         result = 'draw';
+        // draws++;
     } else if (pick === boot) {
         if (opponentPick === 'roach') {
             result = 'win';
+            // wins++;
         } else {
             result = 'loss';
+            // losses++;
         }
     } else if (pick === roach) {
         if (opponentPick === 'nuke') {
@@ -54,41 +62,49 @@ function battle(userPick) {
 
 function playAgain() {
     gameState = 'pick';
+    pickDisplay.classList.remove('hidden');
     loadPage();
 }
+
+// const totalDisplay = document.getElementById('total-display');
+// const winsDisplay = document.getElementById('wins-display');
+// const lossesDisplay = document.getElementById('losses-display');
+// const drawsDisplay = document.getElementById('draws-display');
+
+// function displayScoreboard() {
+//     totalDisplay.textContent = wins + losses + draws;
+//     winsDisplay.textContent = wins;
+//     lossesDisplay.textContent = losses;
+//     drawsDisplay.textContent = draws;
+// }
 
 const boot = document.getElementById('pick-boot');
 const roach = document.getElementById('pick-roach');
 const nuke = document.getElementById('pick-nuke');
 
-const opponentImage = document.getElementById('opponent-image');
-const opponentDisplay = document.getElementById('opponent-display');
+// const opponentImage = document.getElementById('opponent-image');
+// const opponentDisplay = document.getElementById('opponent-display');
 const rematchButton = document.getElementById('rematch-button');
-const resultsDisplay = document.getElementById('results-display');
+// const resultsDisplay = document.getElementById('results-display');
 const pickDisplay = document.getElementById('pick-display');
+const results = document.getElementById('results');
 
 // display
 function displayPick() {
     if (gameState === 'pick') {
-        resultsDisplay.classList.add('hidden');
-        opponentImage.classList.add('hidden');
-        opponentDisplay.classList.add('hidden');
-        rematchButton.classList.add('hidden');
-        if (pick === 'boot') {
-            // roach.classList.add('fade-out');
-            // nuke.classList.add('fade-out');
-            boot.classList.add(result);
-        }
-    } else if (gameState === 'results');
+        results.classList.add('hidden');
+        // if (pick === 'boot') {
+        // roach.classList.add('fade-out');
+        // nuke.classList.add('fade-out');
+        // boot.classList.add(result);
+    }
+    if (gameState === 'results');
     displayResults;
 }
 
 function displayResults() {
     if (gameState === 'results') {
-        resultsDisplay.classList.remove('hidden');
-        opponentImage.classList.remove('hidden');
-        opponentDisplay.classList.remove('hidden');
-        rematchButton.classList.remove('hidden');
+        results.classList.remove('hidden');
         pickDisplay.classList.add('hidden');
     } else if (gameState === 'pick');
     displayPick;
